fb3ce9564aaa1fd037c01e4eab98de4bc896a641
Oren Yaar <oren.yaar@humansecurity.com>
2024-10-28 10:24:54 +0200

    v3.0.6

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let PerimeterX_SDK = Target.binaryTarget(
   name: "PerimeterX_SDK",
-  url: "https://github.com/PerimeterX/px-iOS-Framework/releases/download/3.0.5/PerimeterX_SDK.xcframework.zip",
-  checksum: "13193e095039835bd2b4e49352b875789c5de83ad9d96d26944e2d43727fcc67"
+  url: "https://github.com/PerimeterX/px-iOS-Framework/releases/download/3.0.6/PerimeterX_SDK.xcframework.zip",
+  checksum: "5c1e29834712521bec039c75c31640b8d685318e78ffd3655d3404cc524e4f6c"
 )
 
 let package = Package(
